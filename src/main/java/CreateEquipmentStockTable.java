fa71799dd82e7aa31e26769fc984f48e26db5f62
Jason <clone>
2024-03-27 05:43:09 +0800

    v4

--- src/main/java/CreateEquipmentStockTable.java
+++ src/main/java/CreateEquipmentStockTable.java
@@ -1,18 +1,18 @@
-import ict.itp4511_assignment.db.EquipmentStockDB;
-
-/**
- * @Author: Kong Kwok Hin
- * @Date: 2024/3/27 - 03 - 27 - 上午 04:13
- * @Description: PACKAGE_NAME
- * @version: 1.0
- */
-public class CreateEquipmentStockTable {
-    public static void main(String[] args) {
-        String dbUrl = "jdbc:mysql://localhost:3306/ITP4511_DB?useSSL=false";
-        String dbUser = "root";
-        String dbPassword = "root";
-        EquipmentStockDB equipmentStockDB = new EquipmentStockDB(dbUrl, dbUser, dbPassword);
-        equipmentStockDB.createEquipmentStockTable();
-        equipmentStockDB.insertEquipmentStockRecord();
-    }
-}
+//import ict.itp4511_assignment.db.EquipmentStockDB;
+//
+///**
+// * @Author: Kong Kwok Hin
+// * @Date: 2024/3/27 - 03 - 27 - 上午 04:13
+// * @Description: PACKAGE_NAME
+// * @version: 1.0
+// */
+//public class CreateEquipmentStockTable {
+//    public static void main(String[] args) {
+//        String dbUrl = "jdbc:mysql://localhost:3306/ITP4511_DB?useSSL=false";
+//        String dbUser = "root";
+//        String dbPassword = "root";
+//        EquipmentStockDB equipmentStockDB = new EquipmentStockDB(dbUrl, dbUser, dbPassword);
+//        equipmentStockDB.createEquipmentStockTable();
+//        equipmentStockDB.insertEquipmentStockRecord();
+//    }
+//}
